190fca940d66c9fa8fdf8eb2291f1e8d641717bc
pinstripezebra <seelcs12@gmail.com>
2025-07-18 17:47:04 -0700

    adding method for fetching all app_id's:names from steam api

--- src/query_api.py
+++ src/query_api.py
@@ -1,3 +1,34 @@
 import pandas as pd
+import requests
 
-df = pd.
+
+
+
+def get_all_app_id():
+
+    """Fetch all app IDs and names from Steam API"""
+    
+    req = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2/")
+    data = req.json()
+    apps_data = data['applist']['apps']
+    apps_ids = []
+    app_names = []
+
+    for app in apps_data:
+        appid = app['appid']
+        name = app['name']
+        
+        # skip apps that have empty name
+        if not name:
+            continue
+
+        apps_ids.append(appid)
+        app_names.append(name)
+
+    return pd.DataFrame({
+        'appid': apps_ids,
+        'name': app_names
+    })
+
+test_df = get_all_app_id()
+print(test_df.head(10))
